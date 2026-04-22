5c6b44b5d81eb8acd46bafe3a87fc1e0816b5641
alejoalbornoz <alejoalbornoz912@gmail.com>
2026-04-22 10:06:31 -0300

    first commit

---
+++ SupplementDistributor/src/main/java/com/SupplementDistributor/SupplementDistributor/SupplementDistributorApplication.java
@@ -0,0 +1,13 @@
+package com.SupplementDistributor.SupplementDistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementDistributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplementDistributorApplication.class, args);
+	}
+
+}
---
+++ SupplementDistributor/src/test/java/com/SupplementDistributor/SupplementDistributor/SupplementDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SupplementDistributor.SupplementDistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplementDistributorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
