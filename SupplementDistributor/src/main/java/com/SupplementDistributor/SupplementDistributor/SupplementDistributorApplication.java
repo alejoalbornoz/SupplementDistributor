d7743900335bf871aa3f3827c343ba40c34a5ee9
alejoalbornoz <alejoalbornoz912@gmail.com>
2026-05-06 16:56:33 -0300

    Feat: Add TimeZone for fix problem

--- SupplementDistributor/src/main/java/com/SupplementDistributor/SupplementDistributor/SupplementDistributorApplication.java
+++ SupplementDistributor/src/main/java/com/SupplementDistributor/SupplementDistributor/SupplementDistributorApplication.java
@@ -3,11 +3,14 @@ package com.SupplementDistributor.SupplementDistributor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SupplementDistributorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SupplementDistributorApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(SupplementDistributorApplication.class, args);
 	}
 
 }
